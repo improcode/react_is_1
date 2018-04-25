5a91ea156abb0eb220d12f201d08384cc7a15026
improcode <art.milkowski@gmail.com>
2018-04-25 12:01:41 +0200

    start

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import MyFirstComponent from './components/MyFirstComponent'
+import Add from './components/Add'
+import Header from './components/Header'
 
 const App = () => (
     <div>
-<MyFirstComponent name="Artur" age="28" />
+        <Header text={'PAAANIE, daj pan spokój'}/>
+        <MyFirstComponent name="Artur" age="28"/>
+        <Add numberA={5} numberB={10}/>
     </div>
 )
 
